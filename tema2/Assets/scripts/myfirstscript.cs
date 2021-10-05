021a68fecad01c0fff08aefccf09893a70ea961f
Nicolau Guardiola <nico.guardiolat@gmail.com>
2021-10-05 11:07:57 +0200

    scripts

--- tema2/Assets/scripts/myfirstscript.cs
+++ tema2/Assets/scripts/myfirstscript.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class myfirstscript : MonoBehaviour
 {
+    /*
+    public int playerAge = 35;
+    private float playerSpeed = 5.25f;
+    public string playerName = "Lyu";
+    [SerializeField]
+    private bool gameOver;
+  */
+
+    [SerializeField] private int x = 5;
+    [SerializeField] private int y = 3;
+    [SerializeField] private int z = 3;
+    public string playerName = "Lyu";
+    public string enemyName = "Lyu";
+    /*
+    [SerializeField] private bool isRaining;
+    */
     // Start is called before the first frame update
     void Start()
     {
+        /*
+        Debug.Log(message: x + y);
         
+        if (playerName == enemyName)
+        {
+            Debug.Log(message: "player y enemigo se llaman igual");
+        }
+        if (playerName != enemyName)
+        {
+            Debug.Log(message: "player y enemigo tienen distintos nombres");
+        }
+        */
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        /*
+        if (isRaining)
+        {
+            Debug.Log(message: "Llevate un paraguas");
+        }
+        */
     }
 }
--- tema2/Assets/scripts/vehicle.cs
+++ tema2/Assets/scripts/vehicle.cs
@@ -4,9 +4,44 @@ using UnityEngine;
 
 public class vehicle : MonoBehaviour
 {
+    /*
+    //sound guarda el sonido que hace el vehículo
+    //name guarda los nombres de los vehiculos
+    //numberWheels guarda el numero de ruedas por vehiculo
+    
+    [SerializeField] private int x = 5;
+    [SerializeField] private int y = 3;
+    */
+    [SerializeField] private string name = "";
+    [SerializeField] private int numberWheels = 4;
+    [SerializeField] private string sound = "Brum, Brum";
+    public bool hasSiren;
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
+        /*
+        Debug.Log(message: string.Format("Suma: {0} + {1} = {2}", x, y, x + y));
+        Debug.Log(message: string.Format("Resta: {0} - {1} = {2}", x, y, x-y));
+        Debug.Log(message: string.Format("Producto: {0} * {1} = {2}", x, y, x * y));
+        Debug.Log(message: string.Format("Division: {0} / {1} = {2}", x , y, x / y));
+        
+        Debug.Log(sound);
+        Debug.Log($"hello {sound}");
+        */
+
+        Debug.Log($"{name} tiene {numberWheels} ruedas y hace {sound}");
+        //si hasSiren es true se muestra el mensage: nombre tiene sirena
+       
+        if (hasSiren)
+        {
+            Debug.Log($"{name} tiene sirena");
+        }
+
+
         
     }
 
